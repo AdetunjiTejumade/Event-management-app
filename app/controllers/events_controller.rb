580ff5710fc8a7b4cf209130e936192d6b8387da
AdetunjiTejumade <tejumadeadetunji@gmail.com>
2021-07-04 04:00:05 +0900

    add edit view to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,35 @@
+# frozen_string_literal: true
 class EventsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @events = Event.all
+    @events = Event.where(user_id: current_user.id)
+  end
+
+  def show
+    object = Event.find(params[:id])
+    if current_user.id == object.user_id
+      @event = Event.find(params[:id])
+    else
+      render "#{Rails.root}/public/404.html", status: 404
+    end
   end
 
   def new
     @event = Event.new
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    if @event.destroy
+      redirect_to events_path
+    else
+      redirect_to events_path
+    end
   end
 
   def create
@@ -19,6 +42,18 @@ class EventsController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+
+  private
 
   def event_params
     params.require(:event).permit(:name, :description, :location, :start_date, :end_date, :start_time, :end_time, :paid)
