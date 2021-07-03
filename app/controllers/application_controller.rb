6e9094ae259e76e53ad7482ff8720ecb866c6003
AdetunjiTejumade <tejumadeadetunji@gmail.com>
2021-07-03 22:04:53 +0900

    add authentication

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:first_name, :last_name, :email, :password, :current_password)
+    end
+  end
+end
